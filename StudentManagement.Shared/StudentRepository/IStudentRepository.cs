1277f22c9beab0dce16f294260e806d0aad51a36
DuyhtbSW <duyhtbde160145@fpt.edu.vn>
2024-05-23 20:19:53 +0700

    First Commit, Create DB first + Model Student
    First Commit, Create DB first + Model Student

---
+++ StudentManagement.Shared/StudentRepository/IStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using StudentManagement.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Shared.StudentRepository
+{
+    public interface IStudentRepository
+    {
+        Task<Student> AddStudentAsync (Student student);
+        Task<Student> UpdateStudentAsync (Student student);
+        Task<Student> DeleteStudentAsync (int studentId);  
+        Task<List<Student>> GetAllStudentsAsync ();
+        Task<Student>GetStudentByIdAsync (int studentId);
+
+
+    }
+}
---
+++ StudentManagement/StudentManagement/Services/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+using StudentManagement.Services;
+using StudentManagement.Shared.Models;
+using StudentManagement.Shared.StudentRepository;
+
+namespace StudentManagement.Services
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public StudentRepository(ApplicationDbContext context) { 
+        db = context;
+        }
+        public async Task<Student> AddStudentAsync(Student student)
+        {
+            if (student == null) throw new ArgumentNullException();
+            var newstudent = db.Add(student).Entity;
+            await db.SaveChangesAsync();
+            return newstudent;
+        }
+
+        public async Task<Student?> DeleteStudentAsync(int studentId)
+        {
+           var student = await db.Students.Where(s => s.Id == studentId).FirstOrDefaultAsync();
+            if (student == null)
+                return null;
+            db.Students.Remove(student);
+            await db.SaveChangesAsync();
+            return student;
+        }
+
+        public async Task<List<Student>> GetAllStudentsAsync()
+        {
+            var students = await db.Students.ToListAsync();
+            return students;
+        }
+
+        public Task<Student> GetStudentByIdAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> UpdateStudentAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
